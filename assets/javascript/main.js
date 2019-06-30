345d484676a3943d0aa03a2352df2ddbf3c7b71c
Ryan Hazzard <rhazzardphoto@gmail.com>
2019-06-30 09:48:35 -0700

    broken

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -7,16 +7,20 @@ var queryUrl;
 $('#submit-button').on('click', function(){
 var search = $('#user-input').val().trim()
 console.log(search)
-queryUrl = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=ZbNgxU0AVUxxbLot5nbWFp4cgL5Sqdhu&limit=15`
+queryUrl = `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=ZbNgxU0AVUxxbLot5nbWFp4cgL5Sqdhu&limit=10`
 axios({
     url: queryUrl,
     method: "GET"
 })
     .then(function (response) {
         console.log(response)
-        console.log(response.data.data[0].source)
-        $('#gifs').append(`<div><img src="${response.data.data[0].source}"></img></div>`)
+        console.log(response.data.data)
+        var responseArr = response.data.data
+        for (var i = 0; i < responseArr.length; i++) {
+            $('#gifs').append(`<img id= "gif-${i}" src="${response.data.data[i].url}" ></img>`)
+        }
         })
+        
     .catch(function (err) {
         console.error(err)
     })
