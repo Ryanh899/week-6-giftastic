7b2fc08f7be141f938c0f3d50848ae5ca5270e38
Ryan Hazzard <rhazzardphoto@gmail.com>
2019-06-30 16:25:52 -0700

    some buttons dont work

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,27 +1,54 @@
-var queryUrl; 
+var queryUrl;
+var gifArr = ['Michael Scott', 'Creed Bratton', 'Pam Halpert'];
 
-
-
-
-
-$('#submit-button').on('click', function(){
-var search = $('#user-input').val().trim()
-console.log(search)
-queryUrl = `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=ZbNgxU0AVUxxbLot5nbWFp4cgL5Sqdhu&limit=10`
-axios({
-    url: queryUrl,
-    method: "GET"
+function button() {
+    for (var i = 0; i < gifArr.length; i++) {
+        $('#buttons').append(`<button class="btn btn-dark m-2 button" id="${gifArr[i]}" >${gifArr[i]}</button>`)
+    }
+}
+button()
+$('#submit-button').on('click', function () {
+    event.preventDefault()
+    $('#buttons').empty();
+    var userInput = $('#user-input').val();
+    gifArr.push(userInput);
+    button()
+    $('#user-input').val('')
 })
-    .then(function (response) {
-        console.log(response)
-        console.log(response.data.data)
-        var responseArr = response.data.data
-        for (var i = 0; i < responseArr.length; i++) {
-            $('#gifs').append(`<img id= "gif-${i}" src="${response.data.data[i].url}" ></img>`)
-        }
-        })
-        
-    .catch(function (err) {
-        console.error(err)
+
+$('#buttons').on('click', function () {
+    event.preventDefault()
+    var search = $(event.target).attr('id').trim()
+    queryUrl = `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=ZbNgxU0AVUxxbLot5nbWFp4cgL5Sqdhu&limit=10`
+    axios({
+        url: queryUrl,
+        method: "GET"
     })
+        .then(function (response) {
+            console.log(response.data)
+            var responseArr = response.data.data
+            for (var i = 0; i < responseArr.length; i++) {
+                $('#gifs').append(`<img id= "gif" data-state="still" type="gif" class="image-fluid fixed-bottom pics" data-still="${response.data.data[i].images.fixed_width_still.url}" data-animate="${response.data.data[i].images.fixed_width.url}" src="${response.data.data[i].images.fixed_width_still.url}" ></img>`)
+            }
+        })
+        .catch(function (err) {
+            alert('Pick Again')
+            console.error(err)
+        })
+    $('#gifs').empty()
+})
+var animate;
+var still
+$('#gifs').on('click', function () {
+    animate = $(event.target).attr('data-animate')
+    still = $(event.target).attr('data-still')
+    if ($(event.target).attr('src') === animate) {
+        console.log('animate')
+        $(event.target).attr('src', still)
+        $(event.target).attr('data-state', 'animate')
+    } else if ($(event.target).attr('src') === still) {
+        console.log('still')
+        $(event.target).attr('src', animate)
+        $(event.target).attr('data-state', 'still')
+    }
 })
